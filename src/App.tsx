e2a887862edcfd36cac9548ff55e0a6b61aef6a1
I65090 <Arvind.KS@microchip.com>
2024-07-30 13:40:49 +0530

    removed old HTML files and Added react files

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+// import logo from './logo.svg';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {"This is a react project"}
+    </div>
+  );
+}
+
+export default App;
